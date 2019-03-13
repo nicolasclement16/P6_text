fce7d551d336465b28eaf6356e6d30c3cfbd9765
nicolasclement16 <nicolas.clement16@yahoo.fr>
2019-03-13 15:41:21 +0100

    ajout du fichier run.py

--- engineapp/views.py
+++ engineapp/views.py
@@ -5,3 +5,27 @@ Created on Wed Mar 13 15:15:47 2019
 @author: Nicolas
 """
 
+import os
+import pickle
+
+from os import path as os_path
+from flask import Flask, render_template, redirect, request, url_for
+
+
+
+AUTO_PATH = os_path.abspath(os_path.split(__file__)[0])
+LOCAL_PATH =  'C:/users/Nicolas/37.5/perso - Documents/Formation Data Scientist/Parcours Data Scientist/P6 - Texte/P6_text'
+PATH = LOCAL_PATH
+
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+app.config.from_object('config')
+
+CT_DIR = app.config['BASESAVE']
+print(CT_DIR)
+def load_obj(name):
+    with open(os.path.join(CT_DIR, name), 'rb') as f:
+        mon_depickler = pickle.Unpickler(f)
+        return mon_depickler.load()
+
